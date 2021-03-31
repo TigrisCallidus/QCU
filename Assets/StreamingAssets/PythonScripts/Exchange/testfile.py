1fa11234a98ee6bd472d43b6b76a9334efb258b4
TigrisCallidus <tigris@gmx.ch>
2021-03-31 09:40:27 +0200

    Last part for the examples and correct gitignore

---
+++ Assets/StreamingAssets/PythonScripts/Exchange/testfile.py
@@ -0,0 +1,10 @@
+print('python file works')
+
+
+
+# Uncomment lines below to test if your qiskit installation is correct
+#from qiskit import(
+#  QuantumCircuit,  
+#  IBMQ)
+
+#print(qiskit.__qiskit_version__)
